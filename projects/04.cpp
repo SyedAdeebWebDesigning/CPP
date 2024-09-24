ee7776d03415e4a49a656b15db9cb0465d5002c0
SyedAdeebWebDesigning <syadeeb@student.iul.ac.in>
2024-09-24 13:59:06 +0530

    24/9

--- projects/04.cpp
+++ projects/04.cpp
@@ -4,19 +4,27 @@
 using namespace std;
 
 class Inline {
-public:
-    inline float square(float x) {
-        return x * x;
+    int num;
+    public:
+
+    void getData() {
+        cout << "Enter num: ";
+        cin >> num;
     }
 
-    inline float cube(float x) {
-        return x * x * x;
+    inline float square() {
+        return num * num;
+    }
+
+    inline float cube() {
+        return num * num * num;
     }
 };
 
 int main() {
     Inline obj; 
-    cout << "Square of number: " << obj.square(4.0) << endl;
-    cout << "Cube of number: " << obj.cube(2.0) << endl;
+    obj.getData();
+    cout << "Square of number: " << obj.square() << endl;
+    cout << "Cube of number: " << obj.cube() << endl;
     return 0;
 }
